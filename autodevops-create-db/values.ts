62384c125c267396a4609b3826ba1c17ca341320
devthejo <jo@surikat.pro>
2022-03-01 16:13:38 +0100

    chore(clean): create-db

--- autodevops-create-db/values.ts
+++ autodevops-create-db/values.ts
@@ -1,28 +1,10 @@
 const {
-  SOCIALGOUV_PRODUCTION,
-  SOCIALGOUV_PREPRODUCTION,
-  KEEP_ALIVE,
-  BRANCH_NAME,
   PG_CREATE_EXTENSIONS,
 } = process.env;
-
-const isProduction = Boolean(SOCIALGOUV_PRODUCTION);
-const isPreProduction = Boolean(SOCIALGOUV_PREPRODUCTION);
-const keepAlive = Boolean(KEEP_ALIVE);
-
-const isDev = !isPreProduction && !isProduction;
-const isRenovate = BRANCH_NAME?.startsWith("renovate")
-const isDestroyable = isDev && !keepAlive;
-
-let ttl = "";
-if(isDestroyable){
-  ttl = isRenovate ? "1d" : "7d";
-}
 
 const pgCreateExtensions = PG_CREATE_EXTENSIONS
 
 const values = {
-  ttl,
   pgCreateExtensions,
 }
 
